5079cd2eb1bba25f9264b7207697dd6d72c415df
Majid Kargar <majidkargar01@gmail.com>
2023-08-14 19:11:30 +0430

    refactor(paths): change file paths
    - change all image files path
    - change silverBox library path

--- src/data/cards.js
+++ src/data/cards.js
@@ -2,42 +2,42 @@ const cards = [
 	{
 		value: 0,
 		showItem: false,
-		src: "../src/assets/images/aot/Eren.webp",
+		src: "./assets/images/aot/Eren.webp",
 	},
 	{
 		value: 1,
 		showItem: false,
-		src: "../src/assets/images/aot/Armin.webp",
+		src: "./assets/images/aot/Armin.webp",
 	},
 	{
 		value: 2,
 		showItem: false,
-		src: "../src/assets/images/aot/Historia.webp",
+		src: "./assets/images/aot/Historia.webp",
 	},
 	{
 		value: 3,
 		showItem: false,
-		src: "../src/assets/images/aot/levi.webp",
+		src: "./assets/images/aot/Levi.webp",
 	},
 	{
 		value: 4,
 		showItem: false,
-		src: "../src/assets/images/aot/Mikasa.webp",
+		src: "./assets/images/aot/Mikasa.webp",
 	},
 	{
 		value: 5,
 		showItem: false,
-		src: "../src/assets/images/aot/Reiner.webp",
+		src: "./assets/images/aot/Reiner.webp",
 	},
 	{
 		value: 6,
 		showItem: false,
-		src: "../src/assets/images/aot/Hange.webp",
+		src: "./assets/images/aot/Hange.webp",
 	},
 	{
 		value: 7,
 		showItem: false,
-		src: "../src/assets/images/aot/Erwin.webp",
+		src: "./assets/images/aot/Erwin.webp",
 	},
 ];
 
